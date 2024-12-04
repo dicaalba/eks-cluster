f1c9307d255092e6012ef07ecad424f35badecee
Diana Alfaro <dicaalba@gmail.com>
2024-12-03 23:24:01 -0500

    fix: kubectl install

--- ec2_user_data.sh
+++ ec2_user_data.sh
@@ -77,7 +77,11 @@ install_if_not_exists docker "curl -fsSL https://get.docker.com -o get-docker.sh
 install_if_not_exists docker-compose "curl -L 'https://github.com/docker/compose/releases/download/v2.20.0/docker-compose-$(uname -s)-$(uname -m)' -o /usr/local/bin/docker-compose | sudo chmod +x /usr/local/bin/docker-compose | docker-compose --version"
 
 # Instalar kubectl
-install_if_not_exists kubectl "curl -LO 'https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl' | sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl | kubectl version --client"
+install_if_not_exists kubectl "
+    curl -LO 'https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl' && \
+    sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl && \
+    kubectl version --client
+"
 
 # Instalar eksctl
 install_if_not_exists eksctl "curl --silent --location 'https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz' | tar xz -C /tmp && sudo mv /tmp/eksctl /usr/local/bin || handle_error 'Error al descargar o instalar eksctl'"
